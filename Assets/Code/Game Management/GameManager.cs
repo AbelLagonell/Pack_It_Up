b85cb82afc0f28d6b2fd73ad06160e71ec798f6c
AbelLagonell <alagonell1730@floridapoly.edu>
2025-03-31 15:42:56 -0400

    Added the Timer functionality TODO: No pausing of timer yet made

--- Assets/Code/Game Management/GameManager.cs
+++ Assets/Code/Game Management/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using NETWORK_ENGINE;
@@ -8,10 +9,14 @@ struct GameManagerFlags {
 }
 
 public class GameManager : NetworkComponent {
-    private bool _gameStart = false;
-    private bool _gameOver = false;
+    [SerializeField] private float globalTimer = 70; //in seconds
+
+    private bool _gameStart;
+    private bool _gameOver;
+    private bool _gamePaused;
     private int _robberScore = 10;
     private int _informantScore = 0;
+
 
     public override void HandleMessage(string flag, string value) {
         switch (flag) {
@@ -20,7 +25,7 @@ public class GameManager : NetworkComponent {
                 if (IsClient) {
                     _gameStart = true;
                     foreach (NetworkPlayerManager npm in GameObject.FindObjectsByType<NetworkPlayerManager>(
-                                  FindObjectsSortMode.None)) {
+                                 FindObjectsSortMode.None)) {
                         //TODO Hide Choosing color visuals
                         npm.GameStart();
                     }
@@ -32,7 +37,7 @@ public class GameManager : NetworkComponent {
                     Debug.Log("Game Over");
                     _gameOver = true;
                     foreach (NetworkPlayerManager npm in GameObject.FindObjectsByType<NetworkPlayerManager>(
-                                  FindObjectsSortMode.None)) {
+                                 FindObjectsSortMode.None)) {
                         npm.GameEnd();
                     }
                     //TODO do client stuff
@@ -60,48 +65,52 @@ public class GameManager : NetworkComponent {
 
             npms = FindObjectsByType<NetworkPlayerManager>(FindObjectsSortMode.None);
             foreach (var player in npms) {
+                GameObject temp = MyCore.NetCreateObject(0, player.Owner, Vector3.zero, Quaternion.identity);
                 //TODO create player
             }
-            
+
             //Setting informant
             //TODO Make this a random gen from 1 to the amount of players that there are and then have that be the informant
             npms[0].SetInformant();
-            yield return new WaitForSeconds(0.5f);
-            
+            yield return new WaitForSeconds(1f);
+
+            _gameStart = true;
             SendUpdate(GameManagerFlags.GAMESTART, "1");
             MyCore.StopListening();
-            
+
             //So that the players can read their roles
             yield return new WaitForSeconds(2f);
             foreach (var player in npms) {
                 player.ToggleRoleScreen(false);
+                
             }
-            
-            /*while (!_gameOver) {
+
+            while (!_gameOver) {
+                if (globalTimer < 0) {
+                    //Timer is over here
+                    _gameOver = true;
+                }
+
                 //TODO do game logic here
                 yield return new WaitForSeconds(.1f);
-            }*/
-            
-            yield return new WaitForSeconds(1f);
-            
+            }
+
             //After Game ends but before score screen
             foreach (var player in npms) {
                 player.UpdateRScore(_robberScore);
                 player.UpdateIScore(_informantScore);
                 player.OverrideWinner();
             }
-            
+
             SendUpdate(GameManagerFlags.GAMEOVER, "1");
-            yield return new WaitForSeconds(15f);
-            
+            yield return new WaitForSeconds(5f);
+
             MyId.NotifyDirty();
             StartCoroutine(MyCore.DisconnectServer());
         }
     }
 
-    // Start is called before the first frame update
-    void Start() { }
-
-    // Update is called once per frame
-    void Update() { }
+    private void Update() {
+        if (_gameStart && !_gamePaused) globalTimer -= Time.deltaTime;
+    }
 }
