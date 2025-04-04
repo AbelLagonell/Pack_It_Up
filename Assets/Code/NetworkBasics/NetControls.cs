c91e097679bc8ebaa2c6592cad7abbe9a0833278
Abel-Lagonell <alagonell1730@floridapoly.edu>
2025-04-03 21:32:35 -0400

    Added the bag interactions and money interactions

--- Assets/Code/NetworkBasics/NetControls.cs
+++ Assets/Code/NetworkBasics/NetControls.cs
@@ -12,13 +12,35 @@ struct NetControlFlag {
     public const string SECONDARY = "Secondary";
 }
 
+public enum PrimaryActions {
+    PickupItem,
+    PickupBag,
+    Interaction,
+    Arrest
+}
+
+enum SecondaryActions {
+    Attack,
+    Release,
+    Tamper
+}
+
+
 [RequireComponent(typeof(Rigidbody), typeof(PlayerInput))]
 public class NetControls : NetworkComponent {
+    private Camera cam;
     private Rigidbody _rb;
-    [SerializeField] private float angularSpeed = 5f;
+    public Player _player;
+
+    [SerializeField] private float rayCastDistance = 2.5f;
     [SerializeField] private float linearSpeed = 5f;
 
+    //Server only
+    public Item _item;
+
     //Sync Vars
+    public PrimaryActions _pAction;
+    private SecondaryActions _sAction;
     private Vector2 _lastMoveInput;
     private Vector2 _lastLookInput;
 
@@ -51,7 +73,23 @@ public class NetControls : NetworkComponent {
             case NetControlFlag.LOOKINPUT:
                 if (IsServer) {
                     _lastLookInput = Vector2FromString(value);
-                    Debug.Log(_lastLookInput);
+                }
+
+                break;
+            case NetControlFlag.PRIMARY:
+                if (IsServer) {
+                    switch (_pAction) {
+                        case PrimaryActions.PickupItem:
+                            
+                            if (!_player.hasBag) return;
+                            _player.AddItem(_item);
+                            break;
+                        case PrimaryActions.PickupBag:
+                            if (_player.hasBag) return;
+                            Debug.Log(_player);
+                            _player.AssignBag((Bag)_item);
+                            break;
+                    }
                 }
 
                 break;
@@ -73,20 +111,37 @@ public class NetControls : NetworkComponent {
 
 
     public void OnLookAction(InputAction.CallbackContext lk) {
-        Debug.Log(lk.control.device.);
         if (IsServer) return;
         if (lk.performed || lk.started) {
-            
-            SendCommand(NetControlFlag.LOOKINPUT, new Vector2(_lastLookInput.x, _lastLookInput.y).normalized.ToString());
+            var lkVec = lk.ReadValue<Vector2>();
+            if (lk.control.device is Mouse) {
+                lkVec.x /= Screen.width;
+                lkVec.y /= Screen.height;
+                lkVec.x -= 0.50f;
+                lkVec.y -= 0.50f;
+            } else
+                lkVec = lkVec.normalized;
+
+            lkVec.y = -lkVec.y;
+            SendCommand(NetControlFlag.LOOKINPUT, lkVec.ToString());
         }
 
         if (lk.canceled) {
-            SendCommand(NetControlFlag.LOOKINPUT, Vector2.zero.ToString());
+            SendCommand(NetControlFlag.LOOKINPUT, _lastLookInput.ToString());
+        }
+    }
+
+    public void OnPrimaryAction(InputAction.CallbackContext pa) {
+        if (IsServer) return;
+        if (pa.performed || pa.started) {
+            SendCommand(NetControlFlag.PRIMARY, "");
         }
     }
 
     private void Start() {
         _rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        _player = GetComponent<Player>();
     }
 
     private void Update() {
@@ -95,6 +150,27 @@ public class NetControls : NetworkComponent {
             //Change look rot
             float angle = Mathf.Atan2(_lastLookInput.x, _lastLookInput.y) * Mathf.Rad2Deg;
             _rb.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            _pAction = PrimaryActions.Interaction;
+
+            Ray ray = new Ray(transform.position, -transform.up);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, rayCastDistance)) {
+                switch (hit.collider.tag) {
+                    case "Item":
+                        _pAction = PrimaryActions.PickupItem;
+                        _item = hit.collider.gameObject.GetComponent<Item>();
+                        break;
+                    case "Bag":
+                        _pAction = PrimaryActions.PickupBag;
+                        _item = hit.collider.gameObject.GetComponent<Bag>();
+                        break;
+                }
+            }
+        }
+
+        if (IsLocalPlayer) {
+            cam.transform.position = transform.position - Vector3.forward * 5f;
         }
 
         //Animation
